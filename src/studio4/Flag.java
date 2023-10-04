b94a1712f4c3040b118e54f69ba2939e497440fa
enochlai <enochlai@localhost>
2023-10-04 15:47:10 -0500

    COMMIT AND PUSH STUDIO 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,59 @@
 package studio4;
 
 import java.awt.Color;
+import java.util.Scanner;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in); //making Scanner with a File
+		double x = 0.5;
+		double y = 0.5;
+		double radius = 0.5;
+		
+		StdDraw.circle(x, y, radius);
+		
+		{
+		
+		while (radius > 0.30)
+		{
+		StdDraw.setPenColor(255, 0, 0);
+		StdDraw.circle(x, y, radius);
+		radius = radius - 0.001;
+		}
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.circle(x, y, radius);
+		
+		double halfLength = 0.30;
+		
+		while (halfLength > 0.10)
+		{
+		StdDraw.setPenColor(0, 255, 0);
+		StdDraw.square(x, y, halfLength);
+		halfLength = halfLength - 0.001;
+		}
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.square(x, y, halfLength);
+		
+		radius = halfLength;
+		
+		while (radius > 0)
+		{
+		StdDraw.setPenColor(0, 0, 255);
+		StdDraw.circle(x, y, radius);
+		radius = radius - 0.001;
+		}
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.circle(x, y, radius);
+		
+		
+		}
+		
+		// StdDraw.rectangle(x, y, halfWidth, halfHeight);
+		// circle(double x, double y, double radius)
+		// square(double x, double y, double halfLength)
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,24 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next(); 
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt(); 
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+		
+		String a = "rectangle";
+		
+		if (shapeType.equals (a))
+		{
+		StdDraw.clear();
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+		}
+		
 	}
 }
